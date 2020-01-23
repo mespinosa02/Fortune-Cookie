627076de97c7103e636877a3329bca7388896ae8
Michelle Espinosa <michelle.l.espinosa@gmail.com>
2020-01-23 09:44:16 -0800

    This is an app that displays a set of randomized fortunes from a fortune cookie.

--- Fortune Cookie/ViewController.swift
+++ Fortune Cookie/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fortuneLabel: UILabel!
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cookieButton(_ sender: Any) {
+        let messages = [ "You will find adventure!",
+        "Your hard work will soon pay off!", "Good grades are in your future!", " You will be surrounded by great company!"]
+        fortuneLabel.text = messages.randomElement()!
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        fortuneLabel.text = " "
+    }
 }
 
