067e1bf749db1ee547734b2603c77975970af53c
Michelle Espinosa <michelle.l.espinosa@gmail.com>
2020-01-22 19:51:19 -0800

    Initial Commit

---
+++ Fortune Cookie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fortune Cookie
+//
+//  Created by SBAUser on 1/22/20.
+//  Copyright © 2020 Michelle Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
